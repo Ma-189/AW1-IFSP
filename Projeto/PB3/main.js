b05cd1b97e26f1a1d73734520b55948245594fa1
Ma-189 <78057087+Ma-189@users.noreply.github.com>
2021-10-15 17:43:16 -0300

    adicionar arquivos

--- Projeto/PB3/main.js
+++ Projeto/PB3/main.js
@@ -3,9 +3,17 @@ let nav = document.getElementById ('nav');
 
 icon.addEventListener('click', () => {
     if(nav.style.left === '0rem'){
-       nav.style.left = '-21rem';
+        nav.style.left = '-21rem';
     }
     else{
         nav.style.left = '0rem';
     }
 })
+
+function countText() {
+    let text = document.form_main.cont.value;
+    document.getElementById('cDisplay').innerText = text.length;
+    document.getElementById('pDisplay').innerText = text.split(/\s+/).length;
+    document.getElementById('eDisplay').innerText = text.split(/[\s]+/).length;
+    document.getElementById('lDisplay').innerText = text.split(/\n/).length;
+}
