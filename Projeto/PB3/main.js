32642908c1f3d1a0be11ca1a1fea8b2b12b1e477
Ma-189 <78057087+Ma-189@users.noreply.github.com>
2021-10-20 16:50:18 -0300

    adicionar arquivos

--- Projeto/PB3/main.js
+++ Projeto/PB3/main.js
@@ -12,13 +12,12 @@ icon.addEventListener('click', () => {
 
 function countText() {
     let text = document.form_main.cont.value;
-    document.getElementById('cDisplay').innerText = text.length;
+    document.getElementById('cDisplay').innerText = text.replace(/\s/g, '').length;
     document.getElementById('pDisplay').innerText = text.split(/\s+/).length;
-    document.getElementById('eDisplay').innerText = text.split(/[\s]+/).length;
     document.getElementById('lDisplay').innerText = text.split(/\n/).length;
 }
 
-const t = document.getElementById('texto').value('resultado')
+const t = document.getElementById('texto')
 const result = document.getElementById('resultado')
 
 function maiuscula() {
@@ -29,5 +28,15 @@ function minuscula() {
     result.innerText = t.value.toLowerCase()
 }
 
+function primeiraLetrap() {
+    result.innerText = t.value.toLowerCase().replace(/(?:^|\s)\S/g, function(a) {
+        return a.toUpperCase();
+    });
 
+    return result;
+}
+
+function primeiraLetraf() {
+    result.innerText = t.value.toLowerCase().charAt(0).toUpperCase() + t.value.slice(1);
+}
     
